ba56d27785f0b440b5fc9729124cc5929395a9e0
Sergei Schneidmiller <5797221@gmail.com>
2023-03-19 18:28:26 +0300

    dz2 ready

---
+++ dz2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+try
+{
+    Console.WriteLine("Введите число");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Сумма всех цифр числа равна {NumSum(A)}");
+}
+catch
+{
+    Console.WriteLine("Введены некорректные данные");
+}
+
+
+
+
+int NumSum(int A)
+{
+    int remain = 1;
+    int num = 1;
+    int digit = 1;
+    int sum = 0;
+    while (num != 0)
+    {
+        remain = (A / digit) % 10;
+        sum = sum + remain;
+        num = A / digit;
+        digit = digit * 10;
+    }
+return sum;
+}
