1fe40d0a02c05159e55b86beb4d5282764a65c48
Sergei Schneidmiller <5797221@gmail.com>
2023-03-19 18:46:35 +0300

    dz1 ready

---
+++ dz1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+try
+{
+    Console.WriteLine("Введите число A");
+    int A = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число B");
+    uint B = Convert.ToUInt32(Console.ReadLine());
+    Console.WriteLine($"A в степени B равно {ExponentCalc(A, B)}");
+}
+catch
+{
+    Console.WriteLine("Введены неверные данные");
+}
+
+
+
+
+int ExponentCalc(int A, uint B)
+{
+    int result = 1;
+    for (int i = 1; i<=B; i++)
+    {
+        result = result * A;
+    }
+return result;
+}
